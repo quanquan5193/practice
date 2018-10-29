a0d8bfa1fefd04d9785a3bf1938d62525ca3dbd4
quanlucaz <quanlucaz@gmail.com>
2018-10-30 00:01:25 +0700

    Add domain entities for project

---
+++ Shop.Model/Models/Footer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shop.Model.Models
+{
+    [Table("Footers")]
+    // using System.ComponentModel.DataAnnotations.Schema;
+    public class Footer
+    {
+        [Key] //using System.ComponentModel.DataAnnotations;
+        [MaxLength(50)]
+        public string ID { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+    }
+}
